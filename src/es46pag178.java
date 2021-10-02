1eae7abfa575b53df160d70fe5dc9ccc81126a16
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-02 21:24:07 +0000

    es46pag178

---
+++ src/es46pag178.java
@@ -0,0 +1,13 @@
+public class es46pag178 {
+    public static void main(String[] args){
+        //inserire il numero di amici che prendono un gelato con un gusto
+        int amici_gelato_con_un_gusto = 2;
+        //inserire il prezzo di un gelato con un gusto
+        double gelato_con_un_gusto = 1.20;
+        //inserire il numero di amici che prendono un gelato con due gusti
+        int amici_gelato_con_due_gusti = 3;
+        //inserire il prezzo di un gelato con due gusti
+        int gelato_con_due_gusti = 2;
+        System.out.println((amici_gelato_con_un_gusto*gelato_con_un_gusto)+(amici_gelato_con_due_gusti+gelato_con_due_gusti));
+    }
+}
